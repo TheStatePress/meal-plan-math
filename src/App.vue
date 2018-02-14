f98e448cfc99acbc5fdcd896d1944c8c7c05ed93
James Quigley <James.A.Quigley@asu.edu>
2018-02-14 08:52:15 -0700

    Functional vue parts

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,55 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="input-group">
+      <input v-on:change="breakfastsChange" type="range" min="0" max="7" value="3" name="breakfasts" id="numBreakfasts">
+      <input v-on:change="lunchesChange" type="range" min="0" max="7" value="3" name="lunches" id="numLunches">
+      <input v-on:change="dinnersChange" type="range" min="0" max="7" value="3" name="dinners" id="numDinners">
+
+      <select v-on:change="mealPlanChange">
+        <option value="1932.5">Sparky's Favorite</option>
+        <option value="2210">Unlimited</option>
+        <option value="1587.5">Maroon</option>
+        <option value="1177.5">Gold</option>
+      </select>
+    </div>
+
+    <div id="results">
+      <p>Cash Cost: {{cashCost}}</p>
+      <p>Meal Plan Cost: {{mealPlanPrice}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "app",
-  components: {
-    HelloWorld
+  components: {},
+  computed: {
+    cashCost() {
+      return this.$store.getters.cashCost;
+    },
+    mealPlanPrice() {
+      return this.$store.getters.mealPlanPrice;
+    }
+  },
+  methods: {
+    mealPlanChange: function(event) {
+      const select = event.target;
+      const cost = parseFloat(select.options[select.selectedIndex].value);
+      this.$store.dispatch("setMealPlanPrice", cost);
+    },
+    breakfastsChange: function(event) {
+      const range = event.target;
+      this.$store.dispatch("setNumBreakfasts", parseInt(range.value));
+    },
+    lunchesChange: function(event) {
+      const range = event.target;
+      this.$store.dispatch("setNumLunches", parseInt(range.value));
+    },
+    dinnersChange: function(event) {
+      const range = event.target;
+      this.$store.dispatch("setNumDinners", parseInt(range.value));
+    }
   }
 };
 </script>
