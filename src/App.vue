07739929d35ad8d5da2ad733d22a5e118c2b2792
James Quigley <James.A.Quigley@asu.edu>
2018-02-14 15:22:35 -0700

    Add description of how meal plan prices are determined

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
           </div>
         </div>   
         <div class="row">
-          <p>Change the number of each type of meal you eat per week. The cash value will be graphed on the right in <span class="gold">gold.</span> If you have selected more meals than are in a meal plan, it will be displayed in <span class="gray">gray.</span> All other meal plans will be displayed in <span class="maroon">maroon.</span></p>
+          <p>Change the number of each type of meal you eat per week. The cash value will be graphed on the right in <span class="gold">gold.</span> If you have selected more meals than are in a meal plan, it will be displayed in <span class="gray">gray.</span> All other meal plans will be displayed in <span class="maroon">maroon.</span> Meal plan prices are calculated by subtracting M&amp;G dollars from total price (M&amp;G is 1:1 with cash).</p>
         </div> 
       </div>
 
